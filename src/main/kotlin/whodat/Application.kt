0775bea8abb060de0115dbfe0be922474a7d7d6f
skykanin <3789764+skykanin@users.noreply.github.com>
2025-08-29 16:11:23 +0200

    support client authentication
    endpoints now require that the client
    be authenticated through one of the configured jwks providers.

--- src/main/kotlin/whodat/Application.kt
+++ src/main/kotlin/whodat/Application.kt
@@ -2,7 +2,11 @@ package no.ssb.whodat
 
 import io.micronaut.runtime.Micronaut.run
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.annotations.servers.Server
 
 @OpenAPIDefinition(
@@ -11,9 +15,18 @@ import io.swagger.v3.oas.annotations.servers.Server
             title = "my-openapi-app",
             version = "0.0",
         ),
+    security = [SecurityRequirement(name = "Keycloak token")],
     servers = [
-        Server(url = "http://localhost:8080", description = "Local development"),
+        Server(url = "http://localhost:9191", description = "Local development"),
     ],
+)
+@SecurityScheme(
+    name = "Keycloak token",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "jwt",
+    `in` = SecuritySchemeIn.HEADER,
+    paramName = "Authorization"
 )
 object Api
 
