fec1f48884517c52d656d766480dfdb5ab9f74b3
Michael Moen Allport <mmallport@gmail.com>
2025-09-29 09:27:04 +0200

    Revert changes to 5251b44. Add logging for access token

--- src/main/kotlin/whodat/filters/AccessTokenFilter.kt
+++ src/main/kotlin/whodat/filters/AccessTokenFilter.kt
@@ -8,11 +8,13 @@ import io.micronaut.http.MutableHttpRequest
 import io.micronaut.http.filter.ClientFilterChain
 import io.micronaut.http.filter.HttpClientFilter
 import io.micronaut.inject.qualifiers.Qualifiers
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
 import whodat.filters.AccessTokenFilterMatcher
 import java.io.FileInputStream
+import java.util.*
 import kotlin.jvm.optionals.getOrNull
 
 /**
@@ -23,70 +25,66 @@ import kotlin.jvm.optionals.getOrNull
 @AccessTokenFilterMatcher
 @Singleton
 class AccessTokenFilter(
-    @Value("\${gcp.http.client.filter.credentials-path}") credentialsPath: String?,
-    @param:Value("\${gcp.http.client.filter.project-id}") private val projectId: String?,
+    @Value($$"${gcp.http.client.filter.credentials-path}") credentialsPath: String?,
+    @param:Value($$"${gcp.http.client.filter.project-id}") private val projectId: String?,
     private val applicationContext: ApplicationContext,
 ) : HttpClientFilter {
     private val log = LoggerFactory.getLogger(AccessTokenFilter::class.java)
-    private val baseCredentials: GoogleCredentials =
+    private val credentials: GoogleCredentials
+
+    init {
         if (credentialsPath == null) {
             log.info("Using Application Default Credentials")
-            GoogleCredentials.getApplicationDefault()
+            this.credentials = GoogleCredentials.getApplicationDefault()
         } else {
             log.info("Using Credentials from Service Account file: $credentialsPath")
-            GoogleCredentials.fromStream(FileInputStream(credentialsPath))
+            this.credentials =
+                GoogleCredentials.fromStream(
+                    FileInputStream(credentialsPath),
+                )
         }
-    private val credsWithQuota: GoogleCredentials =
-        if (!projectId.isNullOrBlank()) {
-            baseCredentials.createWithQuotaProject(projectId)
-        } else {
-            baseCredentials
-        }
+    }
 
     override fun doFilter(
         request: MutableHttpRequest<*>,
         chain: ClientFilterChain,
     ): Publisher<out HttpResponse<*>> {
-        val cfg = getConfig(request)
-
-        log.info("Using quota project: ${credsWithQuota.quotaProjectId}")
-
-        val scoped =
-            if (credsWithQuota.createScopedRequired()) {
-                credsWithQuota.createScoped(cfg?.scopes ?: defaultScopesFor(request))
+        val config: AccessTokenFilterConfig? = getConfig(request)
+        val accessToken =
+            if (config != null) {
+                getAccessToken(config.audience)
             } else {
-                credsWithQuota
+                getAccessToken(getAudienceFromRequest(request))
             }
-        log.info("Scoped token: ${scoped.accessToken.tokenValue}")
-        val token = scoped.refreshAccessToken().tokenValue
-        log.info("refreshed token: $token")
-        request.bearerAuth(token)
-        setQuotaProject(request)
+        log.info("Access token: $accessToken")
+        request.bearerAuth(accessToken)
+        setProjectIdHeader(request)
         return chain.proceed(request)
     }
 
+    private fun setProjectIdHeader(request: MutableHttpRequest<*>) {
+        if (projectId != null) {
+            log.debug("Using projectId $projectId from config to override quotaProjectId")
+            request.headers.add("x-goog-user-project", projectId)
+        }
+    }
+
+    private fun getAccessToken(audience: String?): String? = credentials.createScoped(audience).refreshAccessToken().tokenValue
+
     private fun getConfig(request: MutableHttpRequest<*>): AccessTokenFilterConfig? {
         val serviceId = request.getAttribute("micronaut.http.serviceId")
-        return if (serviceId.isPresent) {
-            applicationContext
+        if (serviceId.isPresent) {
+            return applicationContext
                 .findBean(
                     AccessTokenFilterConfig::class.java,
                     Qualifiers.byName(serviceId.get().toString()),
                 ).getOrNull()
-        } else {
-            null
         }
+        return null
     }
 
-    private fun defaultScopesFor(request: MutableHttpRequest<*>): List<String> =
-        listOf("https://www.googleapis.com/auth/cloud-identity.groups.readonly")
-
-    private fun setQuotaProject(request: MutableHttpRequest<*>) {
-        projectId?.let {
-            // Either header…
-            request.headers.add("x-goog-user-project", it)
-            // …or use credentials with quota project:
-            // baseCredentials = baseCredentials.createWithQuotaProject(it)  // if you prefer this route
-        }
+    private fun getAudienceFromRequest(request: MutableHttpRequest<*>): String {
+        val fullURI = request.uri
+        return fullURI.scheme + "://" + fullURI.host
     }
 }
