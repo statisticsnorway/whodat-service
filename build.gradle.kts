ce823a54e02b0addc66913f2c7b43cf3ce9fbb09
skykanin <3789764+skykanin@users.noreply.github.com>
2026-04-09 09:39:16 +0200

    Update Jib base image to debian13

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,7 +104,7 @@ jib {
         jvmFlags = listOf("--add-opens=java.base/java.lang=ALL-UNNAMED")
     }
     from {
-        image = "gcr.io/distroless/java21-debian12@sha256:70e8a4991b6e37cb1eb8eac3b717ed0d68407d1150cf30235d50cd33b2c44f7e"
+        image = "gcr.io/distroless/java21-debian13@sha256:bcdca3804642f766b51c188aa1d080a1bf59bb0dc03361d6cba7636b78597d4e"
         platforms {
             platform {
                 architecture = "amd64"
